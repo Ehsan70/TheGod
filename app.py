7f5ab20696af116f2144352c95c0e51638a0c6c9
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-04-28 16:57:14 -0700

    pushed the messages and user successfully

--- app.py
+++ app.py
@@ -45,11 +45,31 @@ def send_msg(msg):
     return 'Message is %s!' % msg
 
 
+@app.route('/user/<usr>/pushmsg/<msg>')
+def push_msg(usr, msg):
+    # todo make sure the message is not null
+    # todo make sure the user is not null
+
+    msgdata = {
+        "body": msg,
+        "writer": usr
+    }
+    results = db.child('messages').push(msgdata)
+
+    usrdata = {
+        "username": usr,
+        "messages": results
+    }
+    db.child('users').push(usrdata)
+
+    return '{} Message is {}. result is {}!'.format(usr, msg, results)
+
+
 @app.route('/fb')
 def firebase():
     data = {"name": "Ehsa did this"}
     db.child("users").child("Morty").set(data)
-    return "hi ehsan"
+    return "hi ehsan 222"
 
 if __name__ == '__main__':
     app.run()
