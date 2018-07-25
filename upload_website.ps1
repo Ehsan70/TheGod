aef751c3bdcd3eb86a4b331289eb76c129b3de00
seyedehsan.abrishami <seyedehsan.abrishami@visioncritical.com>
2018-07-25 10:05:35 -0700

    upload_website now accepts a profile too

--- upload_website.ps1
+++ upload_website.ps1
@@ -1,7 +1,8 @@
 
 param (
     [Parameter(Mandatory=$true)]
-    [string]$S3Bucket = ""
+    [string]$S3Bucket = "",
+    [string]$AwsProfile = ""
 )
 
 
@@ -9,4 +10,10 @@ if ([string]::IsNullOrEmpty($S3Bucket)){
      throw "S3 bucket have to be specified."
 }
 
-aws s3 cp ./website/ "s3://$S3Bucket" --recursive --exclude "*.yaml" --exclude "*.md"
+if ([string]::IsNullOrEmpty($AwsProfile)){
+    Write-Host "The default profile will be used."
+    aws s3 cp ./website/ "s3://$S3Bucket" --recursive --exclude "*.yaml" --exclude "*.md"
+}else{
+    aws s3 cp ./website/ "s3://$S3Bucket" --recursive --exclude "*.yaml" --exclude "*.md" --profile "$AwsProfile"
+}
+
