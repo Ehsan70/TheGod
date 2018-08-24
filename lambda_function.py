966f147a83581841d61d59323403b0a2619001b2
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-08-24 01:44:12 -0700

    Added comments

--- lambda_function.py
+++ lambda_function.py
@@ -18,16 +18,19 @@ def result(status, message):
 
 def lambda_handler(event, context):
 
-    logger.info('Received event is : {}'.format(json.dumps(event, indent=2)))
+    print('Received event is : {}'.format(json.dumps(event, indent=2)))
 
     # todo make this an env variable using cloudformation
     table = dynamodb.Table('GodMessages')
+    # msgid the url parameter passed to API gateway. All the URL paramteres ap pear under pathParameters
     msgid = event['pathParameters']['msgid']
 
     response = table.get_item(Key={'MessageId':str(msgid)})
     if 'Item' in response:
+        # If there is an Item key in that response then we have found the item
         item = response['Item']
     else:
+        # If there is no Item key in the response then we have not found the item
         return result(400, "Msg not found")
 
     logger.info('Result is : {}'.format(json.dumps(item)))
